da6676600b94687c3958d532e7b9efb63dad2508
Erica Selis <erica.selis@gmail.com>
2015-07-01 12:21:48 -0300

    Hello world using url parameters

--- app.js
+++ app.js
@@ -25,12 +25,20 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 
+app.get('/', function(req, res){
+  res.send('Hello World, From /');
+});
+app.get('/:name', function(req, res){
+  res.send('Hello '+ req.params.name);
+});
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
+
 
 // error handlers
 
